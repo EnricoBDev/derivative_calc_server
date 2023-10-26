c20a1db06dc7b0974aedfd741c99981c1227cbc6
EnricoBDev <enricocubing@gmail.com>
2023-10-26 22:27:08 +0200

    added wwhile loop in server subroutine

--- server.py
+++ server.py
@@ -5,13 +5,14 @@ import websockets
 from derivative_calculator import calculate_point_of_differentiability
 
 async def derivative_process(websocket):
-    # wait for incoming JSON request
-    request = await websocket.recv()
-    request_dict = json.loads(request)
-    response = calculate_point_of_differentiability(request_dict)
-    
-    # send JSON response
-    await websocket.send(response)
+    while True:
+        # wait for incoming JSON request
+        request = await websocket.recv()
+        request_dict = json.loads(request)
+        response = calculate_point_of_differentiability(request_dict)
+        
+        # send JSON response
+        await websocket.send(response)
 
 
 async def main():
